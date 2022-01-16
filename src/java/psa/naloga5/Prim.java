966e463699d140683c910acb65667348303d9c3c
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2022-01-16 22:04:44 +0100

    narejen konstruktor Prim

--- src/java/psa/naloga5/Prim.java
+++ src/java/psa/naloga5/Prim.java
@@ -10,7 +10,38 @@ public class Prim {
 	}
 
 	public Prim(int[][] d) {
-		throw new UnsupportedOperationException("To funkcijo morate implementirati");
+
+		if (d.length == d[0].length) {
+			this.data = d;
+			this.n = d.length;
+		}
+		if (d.length>d[0].length) {
+			int[][] dta = genEmptyData(d.length);
+			for (int i = 0; i < d.length; i++) {
+				for (int j = 0; j < d[0].length; j++) {
+					dta[j][i] = d[j][i];
+				}
+			}
+			this.data = dta;
+		} else if (d[0].length>d.length) {
+			int[][] dta = genEmptyData(d[0].length);
+			for (int i = 0; i < d.length; i++) {
+				for (int j = 0; j < d[0].length; j++) {
+					dta[j][i] = d[j][i];
+				}
+			}
+			this.data = dta;
+		}
+	}
+
+	public int[][] genEmptyData(int n){
+		data = new int[n][n];
+		for (int i = 0; i < data[0].length; i++) {
+			for (int j = 0; j < data.length; j++) {
+				data[i][j] = 0;
+			}
+		}
+		return data;
 	}
 
 	public void addEdge(int i, int j, int d) {
